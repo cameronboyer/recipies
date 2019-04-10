78deea987f1f4181d801a6978f02971c8ee9db75
Cameron Boyer <cameron.boyer@gmail.com>
2019-04-10 14:50:53 -0400

    Update json.js
    add loadign json remove jquery

--- json.js
+++ json.js
@@ -1,25 +1,39 @@
-$().ready(function(){
-    
-    var recipes;
-    $.getJSON( "data.json", function( data ) {
-        console.log(data);
-        recipes = data;
-        $("#text").append(data["name"]);
-    });
-    
-    $("#example-table").tabulator({
-        height:320, // set height of table (optional)
-        fitColumns:true, //fit columns to width of table (optional)
-        columns:[ //Define Table Columns
-            {title:"Name", field:"name", width:150},
-            {title:"Age", field:"age", align:"left", formatter:"progress"},
-            {title:"Favourite Color", field:"col"},
-            {title:"Date Of Birth", field:"dob", sorter:"date", align:"center"},
-        ],
-        rowClick:function(e, row){ //trigger an alert message when the row is clicked
-            alert("Row " + row.getData().id + " Clicked!!!!");
-        },
-    });
+var actualJSON;
 
 
+
+//create Tabulator on DOM element with id "example-table"
+var table = new Tabulator("#example-table", {
+ 	height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+ 	layout:"fitColumns", //fit columns to width of table (optional)
+ 	columns:[ //Define Table Columns
+	 	{title:"Name", field:"name", width:150},
+	 	{title:"Age", field:"age", align:"left", formatter:"progress"},
+	 	{title:"Favourite Color", field:"col"},
+	 	{title:"Date Of Birth", field:"dob", sorter:"date", align:"center"},
+ 	],
+ 	rowClick:function(e, row){ //trigger an alert message when the row is clicked
+ 		alert("Row " + row.getData().id + " Clicked!!!!");
+ 	},
 });
+
+
+ function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'data.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);  
+ }
+
+function init() {
+    loadJSON(function(response) {
+        actualJSON = JSON.parse(response);
+    }
+ }
