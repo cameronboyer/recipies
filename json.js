92baf9487600eb3826d81a959d25fe6a7374d127
Cameron Boyer <cameron.boyer@gmail.com>
2019-04-10 14:35:22 -0400

    Update json.js
    add tabular to js

--- json.js
+++ json.js
@@ -1,7 +1,25 @@
-
 $().ready(function(){
+    
+    var recipes;
     $.getJSON( "data.json", function( data ) {
-    console.log(data);
-    $("#text").append(data["name"]);
-  });
+        console.log(data);
+        recipes = data;
+        $("#text").append(data["name"]);
+    });
+    
+    $("#example-table").tabulator({
+        height:320, // set height of table (optional)
+        fitColumns:true, //fit columns to width of table (optional)
+        columns:[ //Define Table Columns
+            {title:"Name", field:"name", width:150},
+            {title:"Age", field:"age", align:"left", formatter:"progress"},
+            {title:"Favourite Color", field:"col"},
+            {title:"Date Of Birth", field:"dob", sorter:"date", align:"center"},
+        ],
+        rowClick:function(e, row){ //trigger an alert message when the row is clicked
+            alert("Row " + row.getData().id + " Clicked!!!!");
+        },
+    });
+
+
 });
