e390c81537472fdb8169a000c9fa525969760146
Cameron Boyer <cameron.boyer@gmail.com>
2019-04-10 16:09:33 -0400

    Update json.js
    add other method for checking clicked.

--- json.js
+++ json.js
@@ -61,6 +61,8 @@ function createCheckBoxes() {
 }
 
 function getListForTable() {
+	var checkedBoxesOther = getCheckedBoxes("myCheckBox");
+	console.log("other boxes checked:", checkedBoxesOther);
 	var checkedBoxes = document.querySelectorAll('input[name=myCheckBox]:checked');
 	console.log("boxes checked:", checkedBoxes);
 	var data = [];
@@ -73,4 +75,19 @@ function getListForTable() {
 	}
 	console.log("data for table:", data);
 	return data;
+}
+
+
+function getCheckedBoxes(chkboxName) {
+  var checkboxes = document.getElementsByName(chkboxName);
+  var checkboxesChecked = [];
+  // loop over them all
+  for (var i=0; i<checkboxes.length; i++) {
+     // And stick the checked ones onto an array...
+     if (checkboxes[i].checked) {
+        checkboxesChecked.push(checkboxes[i]);
+     }
+  }
+  // Return the array if it is non-empty, or null
+  return checkboxesChecked.length > 0 ? checkboxesChecked : null;
 }
