fba4869abbda3c76fdd62976b2ec6afbb7ce3f57
Cameron Boyer <cameron.boyer@gmail.com>
2019-04-10 14:59:50 -0400

    Update json.js
    add bind to our data

--- json.js
+++ json.js
@@ -6,15 +6,7 @@ var actualJSON;
 var table = new Tabulator("#example-table", {
  	height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
  	layout:"fitColumns", //fit columns to width of table (optional)
- 	columns:[ //Define Table Columns
-	 	{title:"Name", field:"name", width:150},
-	 	{title:"Age", field:"age", align:"left", formatter:"progress"},
-	 	{title:"Favourite Color", field:"col"},
-	 	{title:"Date Of Birth", field:"dob", sorter:"date", align:"center"},
- 	],
- 	rowClick:function(e, row){ //trigger an alert message when the row is clicked
- 		alert("Row " + row.getData().id + " Clicked!!!!");
- 	},
+ 	columns:[ actualJSON ],
 });
 
 
